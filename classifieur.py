e67e573c6e5a6f9bb70bc17e68416c9917f473db
= <vollmerflorent@gmail.com>
2018-11-21 18:17:59 +0100

    Lecture et ecriture auto des barycentres

--- classifieur.py
+++ classifieur.py
@@ -1,8 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
+import scipy.misc
+from PIL import Image
 
 class Classifieur :
+
+	barycentres = []
+
+	def __init__(self, train_data, testSize) :
+		self.td = train_data
+		self.testSize = testSize
+		for i in range(1, 11) :
+			self.barycentreX(i)
 
 	def distanceEntre2images(self, imageBary, imageCible):
 		distanceParcouru = 0
@@ -13,8 +23,17 @@ class Classifieur :
 		distanceParcouru = np.sqrt(distanceParcouru)
 		print(distanceParcouru)
 		
-	def barycentreX(self, train_data, label) : 
-		testSize = 150
-		tableBool = train_data['y'].squeeze(1)[:testSize]==label
-		barycentre = np.mean(train_data['X'][:, :, :, tableBool], axis=3)
-		return barycentre
+	def barycentreX(self, label) :
+		
+		try :
+			img = Image.open('barycentre' + str(label) + '.png')
+		except FileNotFoundError :
+			tableBool = self.td['y'].squeeze(1)[:self.testSize]==label
+			barycentre = np.mean(self.td['X'][:, :, :, tableBool], axis=3)
+			scipy.misc.imsave('barycentre' + str(label) + '.png', barycentre)
+			return barycentre
+		else :
+			return np.array(img)
+		
+		
+		
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from scipy.io import loadmat
 from pretraitement import PreProc
 from classifieur import Classifieur
 
-
 train_data = loadmat('train_32x32.mat')
 test_data = loadmat('test_32x32.mat')
 
@@ -14,16 +13,18 @@ train_data['X'] = train_data['X'][:, :, :, :testSize] # taille d'echantillon a t
 
 
 preProc = PreProc(train_data, testSize)
-preProc.pretraiteDatas();
+preProc.pretraiteDatas()
 
 
 #print(classe1.shape)
 #print(train_data['X'].shape)
-imgTest = np.mean(train_data['X'][:, :, :, classe1], axis=3)
+classifieur = Classifieur(train_data, testSize)
+imgTest = classifieur.barycentreX(2)#np.mean(train_data['X'][:, :, :, classe1], axis=3)
 #print(imgTest.shape)
-classifieur = Classifieur()
 classifieur.distanceEntre2images(imgTest.astype("int"), train_data['X'][:, :, :, 3])
 #plt.imshow(imgTest.astype("int"))
+
+
 plt.imshow(train_data['X'][:, :, :, 3])
 #plt.savefig('foo.png')
 #print('Label', train_data['y'][3])
